9538352788b24b4011fffafb56c0b1ace8d45423
jaredleoa <88289448+jaredleoa@users.noreply.github.com>
2023-12-13 12:50:26 +0000

    .

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ COPY ./requirements.txt /app/requirements.txt
 WORKDIR /app
 RUN pip install -r requirements.txt
 COPY . /app
-ENTRYPOINT [ "python" ]
-CMD ["hello.py" ]
+ENTRYPOINT ["python"]
+CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,48 +1,42 @@
-from flask import Flask
-from flask import request
+from flask import Flask, render_template, request, json
 from flask_mysqldb import MySQL
 from flask_cors import CORS
-import json
-mysql = MySQL()
+
 app = Flask(__name__)
 CORS(app)
-# My SQL Instance configurations
-# Change these details to match your instance configurations
-app.config['MYSQL_USER'] = 'A'
-app.config['MYSQL_PASSWORD'] = 'B'
-app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = 'db.mydomain.ie'
-mysql.init_app(app)
+mysql = MySQL(app)
 
-@app.route("/add") #Add Student
+# MySQL configurations
+app.config['MYSQL_USER'] = 'A'  # Replace with your MySQL user
+app.config['MYSQL_PASSWORD'] = 'B'  # Replace with your MySQL password
+app.config['MYSQL_DB'] = 'student'  # Replace with your database name
+app.config['MYSQL_HOST'] = 'db.mydomain.ie'  # Replace with your MySQL host
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/add", methods=['GET', 'POST'])  # Add Student
 def add():
-  name = request.args.get('name')
-  email = request.args.get('email')
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email) # kludge - use stored proc or params
-  cur.execute(s)
-  mysql.connection.commit()
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO students(studentName, email) VALUES(%s, %s)", (name, email))
+        mysql.connection.commit()
+        cur.close()
+        return json.dumps({'message': 'Student added successfully'})
+    return render_template("add_student.html")
 
-  return '{"Result":"Success"}' # Really? maybe we should check!
-  
-@app.route("/") #Default - Show Data
-def read(): # Name of the method
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  cur.execute('''SELECT * FROM students''') # execute an SQL statment
-  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
-  Results=[]
-  for row in rv: #Format the Output Results and add to return string
-    Result={}
-    Result['Name']=row[0].replace('\n',' ')
-    Result['Email']=row[1]
-    Result['ID']=row[2]
-    Results.append(Result)
-  response={'Results':Results, 'count':len(Results)}
-  ret=app.response_class(
-    response=json.dumps(response),
-    status=200,
-    mimetype='application/json'
-  )
-  return ret #Return the data in a string format
+@app.route("/students")  # Read all students
+def students():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM students")
+    student_data = cur.fetchall()
+    cur.close()
+    return render_template("students.html", students=student_data)
+
+# Additional routes for Update and Delete operations can be added here
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
+    app.run(host='0.0.0.0', port=8080)
